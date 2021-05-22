2978f3038bc2b2ecc4ca01da66a9feebe638d5bd
Alisson Leal <alissonsleal@hotmail.com>
2021-05-22 19:39:36 -0300

    [feat] - autocomplete search

--- components/SearchInput/index.tsx
+++ components/SearchInput/index.tsx
@@ -1,13 +1,18 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import ReactGA from 'react-ga';
 import { FiSearch } from 'react-icons/fi';
 import { useRouter } from 'next/router';
 import api from '@/utils/services/api';
+import apiV2 from '@/utils/services/apiV2';
 import { useGlobal } from '@/stores/GlobalContext';
 import { Container } from './styles';
 import QualitySelection from '../QualitySelection';
 import ErrorModal from '../ErrorModal';
 import LoadingModal from '../LoadingModal';
+
+interface AutoCompleteProps {
+  display_name: string;
+}
 
 const SearchInput = (): any => {
   const router = useRouter();
@@ -21,6 +26,19 @@ const SearchInput = (): any => {
     setVodUrl,
   } = useGlobal();
   const [username, setUsername] = useState('');
+  const [autoComplete, setAutoComplete] = useState<Array<AutoCompleteProps>>(
+    [],
+  );
+
+  useEffect(() => {
+    !!username.length &&
+      apiV2
+        .get(`search/channels?query=${username}&first=3`)
+        .then((response) => {
+          setAutoComplete(response.data.data);
+        })
+        .catch((error) => console.warn(error));
+  }, [username]);
 
   const handleSubmit = () => {
     if (username) {
@@ -28,35 +46,41 @@ const SearchInput = (): any => {
       router.prefetch(`/videos/${username}`);
       try {
         setError('');
-        api.get(`users?login=${username}`).then((response) => {
-          try {
-            api
-              .get(`channels/${response.data.users[0]._id}/videos?limit=1`)
-              .then((channelResponse: any) => {
-                if (channelResponse.data.videos.length !== 0) {
-                  router.push(`/videos/${username}`, undefined, {
-                    scroll: false,
-                  });
+        api
+          .get(`users?login=${username}`)
+          .then((response) => {
+            try {
+              api
+                .get(`channels/${response.data.users[0]._id}/videos?limit=1`)
+                .then((channelResponse: any) => {
+                  if (channelResponse.data.videos.length !== 0) {
+                    router.push(`/videos/${username}`, undefined, {
+                      scroll: false,
+                    });
+                    setVodUrl('');
+                    inputRef.current?.blur();
+                    window.scrollTo({ behavior: 'smooth', top: 340 });
+                  }
+                  if (channelResponse.data._total === 0) {
+                    setError(`${username} does not have any available streams`);
+                  }
+                })
+                .finally(() => {
                   setLoading(false);
-                  setVodUrl('');
-                  inputRef.current?.blur();
-                  window.scrollTo({ behavior: 'smooth', top: 340 });
-                }
-                if (channelResponse.data._total === 0) {
-                  setLoading(false);
-                  setError(`${username} does not have any available streams`);
-                }
-              });
+                });
 
-            ReactGA.event({
-              category: 'SearchedUserForDeletedVod',
-              action: `${username}`,
-            });
-          } catch (err) {
+              ReactGA.event({
+                category: 'SearchedUserForDeletedVod',
+                action: `${username}`,
+              });
+            } catch (err) {
+              setError('This user does not exist or is unavailable');
+            }
+          })
+          .catch((err) => setError(err.response.data.message))
+          .finally(() => {
             setLoading(false);
-            setError('This user does not exist or is unavailable');
-          }
-        });
+          });
       } catch (err) {
         console.warn(err);
         setLoading(false);
@@ -82,7 +106,17 @@ const SearchInput = (): any => {
         onChange={(event) => setUsername(event.target.value)}
         value={username}
         placeholder="Streamer Username"
+        list="streamer-username"
+        autoComplete="off"
       />
+
+      <datalist id="streamer-username">
+        {!!username &&
+          !!autoComplete.length &&
+          autoComplete.map((user: { display_name: string }) => (
+            <option key={user.display_name} value={user.display_name} />
+          ))}
+      </datalist>
       <QualitySelection
         onChange={(event: any) => setVideoQuality(event.target.value)}
       />
