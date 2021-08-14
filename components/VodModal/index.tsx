1962c228777ff5643c5699311595e220f2ca5bee
Alisson Leal <alissonsleal@hotmail.com>
2021-08-14 20:00:40 -0300

    feat: add clip download and playback rate

--- components/VodModal/index.tsx
+++ components/VodModal/index.tsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useRef, useEffect, useMemo, useState } from 'react';
 import ReactGA from 'react-ga';
 import ReactPlayer from 'react-player';
 import { FaWindowClose } from 'react-icons/fa';
 import { Player } from 'video-react';
 
 import { useGlobal } from '@/stores/GlobalContext';
-import { Container, Ads } from './styles';
+import { Container, Ads, CustomOptions } from './styles';
 
 // 7days * 24 hours * 60 minutes * 60 seconds * 1000ms
 const timeInterval = 7 * 24 * 60 * 60 * 1000;
 
 const VodModal = ({ videoUrl, previewUrl }: any) => {
+  const [playbackRate, setPlaybackRate] = useState(1);
+  const playerRef: any = useRef(null);
+
   const { videoQuality } = useGlobal();
   const [showAd, setShowAd] = useState(false);
   useEffect(() => {
@@ -28,6 +31,22 @@ const VodModal = ({ videoUrl, previewUrl }: any) => {
       category: 'AlreadyPaid',
       action: videoUrl.split('_')[1],
     });
+  };
+
+  const downloadClip = () => {
+    const currentTime = playerRef?.current?.getCurrentTime() || 0;
+
+    const clippedPart = Math.floor(currentTime / 10);
+
+    const clippedUrl = videoUrl
+      .replace('index-dvr.m3u8', `${clippedPart}.ts`)
+      .replace(process.env.NEXT_PUBLIC_CORS, '');
+
+    const link = document.createElement('a');
+    link.href = clippedUrl;
+    link.setAttribute('download', 'clip');
+    document.body.appendChild(link);
+    link.click();
   };
 
   const showCustomPlayer =
@@ -76,31 +95,63 @@ const VodModal = ({ videoUrl, previewUrl }: any) => {
             />
           </>
         ) : (
-          <ReactPlayer
-            url={videoUrl}
-            controls
-            width="100%"
-            height="100%"
-            config={{
-              file: {
-                hlsOptions: {
-                  xhrSetup: (xhr: any, _url: string) => {
-                    xhr.open(
-                      'GET',
-                      _url
-                        .replace('unmuted.ts', 'muted.ts')
-                        .replace('chunked', videoQuality),
-                      true,
-                    );
+          <>
+            <CustomOptions>
+              <select
+                defaultValue="1"
+                onChange={(e) => setPlaybackRate(Number(e.target.value))}
+              >
+                <option value="0.5">Speed 0.5x</option>
+                <option value="0.75">Speed 0.75x</option>
+                <option value="1">Speed 1x</option>
+                <option value="2">Speed 2x</option>
+              </select>
+
+              <button onClick={downloadClip}>
+                Clip it{' '}
+                <img
+                  title="LUL"
+                  src="https://static-cdn.jtvnw.net/emoticons/v2/425618/default/dark/3.0"
+                />
+              </button>
+            </CustomOptions>
+
+            <ReactPlayer
+              ref={playerRef}
+              url={videoUrl}
+              controls
+              width="100%"
+              height="100%"
+              playbackRate={playbackRate}
+              config={{
+                file: {
+                  hlsOptions: {
+                    xhrSetup: (xhr: any, _url: string) => {
+                      xhr.open(
+                        'GET',
+                        _url
+                          .replace('unmuted.ts', 'muted.ts')
+                          .replace('chunked', videoQuality),
+                        true,
+                      );
+                    },
                   },
                 },
-              },
-            }}
-          />
+              }}
+            />
+          </>
         )}
       </>
     );
-  }, [videoUrl, showAd, setShowAd, videoQuality]);
+  }, [
+    videoUrl,
+    showAd,
+    setShowAd,
+    videoQuality,
+    setPlaybackRate,
+    playbackRate,
+    downloadClip,
+  ]);
 
   return <Container>{renderVodModal}</Container>;
 };
